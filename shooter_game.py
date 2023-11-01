7661b9c54200d1d2b78549b9ef72f9ff4538ecb6
darkwolf <darkwolf1990janary@gmail.com>
2023-11-01 19:13:09 +0300

    new game shooter update

--- shooter_game.py
+++ shooter_game.py
@@ -1,16 +1,22 @@
 import sys
 
 import pygame
+from random import randint
 
 pygame.init()
+
+game_font = pygame.font.Font(None, 80)
+game_font_ = pygame.font.Font(None, 30)
 
 screen_width, screen_height = 800, 600
 screen_fill_color = (72, 61, 139)
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 pygame.display.set_caption("Awesome Shooter Game")
+pygame.mixer.music.load('music/bensound-lofinerdbassbuzzer.mp3')
+pygame.mixer.music.play(-1)
 
-FIGHTER_STEP = 0.5
+FIGHTER_STEP = 1
 
 fighter_image = pygame.image.load('img/spaceShip.png')
 fighter_width, fighter_height = fighter_image.get_size()
@@ -24,10 +30,17 @@ rocket_width, rocket_height = rocket_image.get_size()
 rocket_X, rocket_y = fighter_x + fighter_width / 2 - rocket_width / 2, fighter_y - rocket_height
 rocket_was_fired = False
 
-pygame.mixer.music.load('music/bensound-lofinerdbassbuzzer.mp3')
-pygame.mixer.music.play(-1)
+ALIEN_STEP = 0.1
+alien_speed = ALIEN_STEP
+alien_image = pygame.image.load('img/alien.png')
+alien_width, alien_height = alien_image.get_size()
+alien_x, alien_y = randint(0, screen_width - alien_width), 0
 
-while True:
+game_is_running = True
+
+game_score = 0
+
+while game_is_running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
@@ -51,6 +64,8 @@ while True:
     if fighter_is_moving_right and fighter_x <= screen_width - fighter_width - FIGHTER_STEP:
         fighter_x += FIGHTER_STEP
 
+    alien_y += alien_speed
+
     if rocket_was_fired and rocket_y + rocket_height < 0:
         rocket_was_fired = False
     if rocket_was_fired:
@@ -58,8 +73,32 @@ while True:
 
     screen.fill(screen_fill_color)
     screen.blit(fighter_image, (fighter_x, fighter_y))
+    screen.blit(alien_image, (alien_x, alien_y))
 
     if rocket_was_fired:
         screen.blit(rocket_image, (rocket_X, rocket_y))
+
+        game_score_text = game_font_.render(f"Your score is {game_score}", True, 'lightgreen')
+        screen.blit(game_score_text, (20, 20))
+
     pygame.display.update()
 
+    if alien_y + alien_height > fighter_y:
+        game_is_running = False
+
+    if rocket_was_fired and\
+            alien_x < rocket_X < alien_x + alien_width - rocket_width and\
+            alien_y < rocket_y + alien_height - rocket_height:
+        rocket_was_fired = False
+        alien_x, alien_y = randint(0, screen_width - alien_width), 0
+        alien_speed += ALIEN_STEP / 2
+        game_score += 1
+
+game_over_text = game_font.render("Game Over", True, 'lightgreen')
+game_over_rectangle = game_over_text.get_rect()
+game_over_rectangle.center = (screen_width / 2, screen_height / 2)
+screen.blit(game_over_text, game_over_rectangle)
+pygame.display.update()
+pygame.time.wait(5000)
+
+pygame.quit()
